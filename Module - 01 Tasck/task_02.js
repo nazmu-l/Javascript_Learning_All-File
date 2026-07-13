ce04efbdb76cdbf21a4e68d34662fc14ccadff6c
Nazmul Hossain <nazmulhasannh4082041@gmail.com>
2026-07-14 03:42:58 +0600

    add file

--- Module - 01 Tasck/task_02.js
+++
@@ -1,9 +0,0 @@
-// Task _ 02 
-
-const math = 75.25 , bio = 65 , che = 80 , phy = 35.45 , ban = 99.50 ; // all Mark Defined
-
-let studentTotalMark = math + bio + che + phy + ban ; // Total mark sum
-
-let averageMark = parseFloat(studentTotalMark / 5); // Get average mark
-
-console.log(averageMark.toFixed(2)) // output .2 decimal number
--- Module - 01 Tasck/task_03.js
+++
@@ -1,6 +0,0 @@
-//task 03
-
-const myNumber = 119 ;
-let divide = myNumber % 5 ;
-
-console.log(divide)

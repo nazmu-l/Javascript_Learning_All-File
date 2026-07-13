d811b54add17003c83de0c1d1dd1e813e97ac996
Nazmul Hossain <nazmulhasannh4082041@gmail.com>
2026-07-14 01:47:45 +0600

    Some add

--- varieble.js
+++ varieble.js
@@ -1,2 +1,7 @@
-let number = 2.355685 ;
-console.log(typeof number)
+var first = 0.1 ;
+var second = 0.2 ;
+var total = first + second ;
+console.log(total.toFixed(1))
+
+var num = 50/0 ;
+console.log(num) 

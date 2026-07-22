8048d02b6c2847fdadbff9374a9aa6acf230f03e
Nazmul Hossain <nazmulhasannh4082041@gmail.com>
2026-07-23 00:49:10 +0600

    indexOf add

--- Js -consuptual section/array.js
+++ Js -consuptual section/array.js
@@ -23,7 +23,9 @@ var number = [
 // number.shift(number) // array 1st element delete in using shift keyword
 // number.unshift(5 , 10 , 20); // array  first index add element using unshift
 
-console.log(number)
+
+
+// console.log(number.indexOf(200)) // index number find
 
 
 
